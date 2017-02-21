b3c9ba4d4c61791ea9fd8e5aaed9d707da65aef2
awwwesssooooome <chengpeng@yixin.im>
2017-02-21 10:56:39 +0800

    rm flex & fix reset

--- qw/src/App.vue
+++ qw/src/App.vue
@@ -1,22 +1,16 @@
 <style>
-body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, dl, dt, dd, ul, ol, li, pre, fieldset, lengend, button, input, textarea, th, td {margin:0;padding:0;}
+body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, dl, dt, dd, ul, ol, li, button, input, textarea, th, td {margin:0;padding:0;}
 body, button, input, select, textarea {font: 12px/1 Avenir, Helvetica, "Mircosoft Yahei", Tahoma, Arial, "\5b8b\4f53", sans-serif;}
 h1, h2, h3, h4, h5, h6 {font-family:Avenir-Black, Helvetica, "Microsoft Yahei"}
 h1 {font-size:18px;}
 h2 {font-size:16px;}
 h3 {font-size:14px;}
 h4, h5, h6 {font-size:100%;}
-address, cite, dfn, em, var {font-style:normal;} 
-code, kbd, pre, samp, tt {font-family: "Courier New", Courier, monospace;}
-small { font-size: 12px; } 
 ul, ol { list-style: none; }
-a { text-decoration: none; }
-a:hover { text-decoration: none; }
-legend { color: #000; }
+a, a:hover { text-decoration: none; }
 fieldset, img { border: none; }
 button, input, select, textarea { font-size: 100%; }
 table {border-collapse:collapse;border-spacing:0;}
-hr {border:none;height:1px;}
 html {overflow-y:scroll;}
 .view {width:1080px;margin:90px auto 0;}
 .article-wrap {float:left;}
@@ -30,7 +24,7 @@ html {overflow-y:scroll;}
 
 <template>
     <div id="app">
-        <navbar></navbar>
+        <navbar v-show="!isMobile"></navbar>
         <router-view class="view"></router-view>
     </div>
 </template>
@@ -40,7 +34,16 @@ import navbar from './components/navbar.vue'
 
 export default {
     name: 'app',
+    data() {
+        return {
+            isMobile: false
+        }
+    },
+    created() {
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            this.isMobile = true;
+        }
+    },
     components: { navbar }
 }
 </script>
-
--- qw/src/components/m/work.vue
+++ qw/src/components/m/work.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import flexible from '../../javascript/flexible.js'
+//import flexible from '../../javascript/flexible.js'
 
 export default {
     data() {
@@ -14,6 +14,8 @@ export default {
         }
     },
     mounted() {
+        this.flexible()
+
         this.$http.jsonp('/res/data/work.json', {
             jsonp: "callback", 
             jsonpCallback: "work"
@@ -23,7 +25,46 @@ export default {
             this.works = response.data.works
         }, response => {
             console.log(response)
-        });
+        })
+    },
+    methods: {
+        flexible() {
+            var h;
+            var dpr = window.navigator.appVersion.match(/iphone/gi)?window.devicePixelRatio:1;
+            var scale = 1 / dpr;
+            var docEl = document.documentElement;
+            var metaEl = document.createElement('meta');
+
+            function setUnitA() {
+                window.rem = docEl.getBoundingClientRect().width / 16;
+                docEl.style.fontSize = window.rem + 'px';
+            }
+
+            window.dpr = dpr;
+            window.addEventListener('resize', function() {
+                clearTimeout(h);
+                h = setTimeout(setUnitA, 300);
+            }, false);
+            window.addEventListener('pageshow', function(e) {
+                if (e.persisted) {
+                    clearTimeout(h);
+                    h = setTimeout(setUnitA, 300);
+                }
+            }, false);
+
+            docEl.setAttribute('data-dpr', dpr);
+            metaEl.setAttribute('name', 'viewport');
+            metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
+            if (docEl.firstElementChild) {
+                docEl.firstElementChild.appendChild(metaEl);
+            } else {
+                var wrap = document.createElement('div');
+                wrap.appendChild(metaEl);
+                document.write(wrap.innerHTML);
+            }
+
+            setUnitA()
+        }
     }
 }
 </script>
