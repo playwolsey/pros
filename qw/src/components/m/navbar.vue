45fdd59202632afe584ea75ead5838c8be3c106a
awwwesssooooome <chengpeng@yixin.im>
2017-02-22 17:01:30 +0800

    detail nav back button added

--- qw/src/components/m/navbar.vue
+++ qw/src/components/m/navbar.vue
@@ -1,6 +1,7 @@
 <style>
 .mnavbar-wrap {width:14.4rem;margin:0 auto;}
 .mnavbar-wrap .mnavbar {font-size:.9rem;line-height:1rem;letter-spacing:1px;padding:.36rem 0;position:relative;border-bottom:8px solid #000000;}
+.mnavbar-wrap .mnavbar .back {width:1.6rem;height:1.6rem;background:url(/res/img/back.png) no-repeat center center;background-size:100% 100%;margin:.7rem 0 .7rem;}
 .mnavbar-wrap .mnavbar .menu {font-size:.3375rem;position:absolute;right:10px;bottom:.33rem;}
 .mnavbar-wrap .mnavbar .menu span {width:.925rem;height:0.1125rem;background:#000;margin-bottom:.15rem;-webkit-transition:all 0.3s ease;-moz-transition:all 0.3s ease;transition:all 0.3s ease;display:block;}
 .mnavbar-wrap .menubox {font-size:1.35rem;line-height:1.35rem;margin-top:.3rem;}
@@ -18,12 +19,15 @@
 <div id="mnavbar">
     <div class="mnavbar-wrap">
         <div class="mnavbar"> 
-            <h1>QUANWEN<br>DESIGN<br>OFFICE.</h1>
-            <div :class="[show ? openClass : '', menuClass]" @click="show=!show">
-                <span></span>
-                <span></span>
-                <span></span>
+            <div v-if="!isDetail">
+                <h1>QUANWEN<br>DESIGN<br>OFFICE.</h1>
+                <div :class="[show ? openClass : '', menuClass]" @click="show=!show">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
+            <div class="back" v-if="isDetail" @click="back"></div>
         </div>
         <transition name="menubox-fade">
             <div class="menubox" v-if="show">
@@ -46,7 +50,27 @@ export default {
         return {
             menuClass: 'menu',
             openClass: 'open',
-            show: false 
+            show: false,
+            isDetail: false
+        }
+    },
+    watch: {
+        '$route': 'getIsDetail'
+    },
+    created() {
+        this.getIsDetail();
+    },
+    methods: {
+        getIsDetail() {
+            let path = this.$route.path;
+            if (path.indexOf('detail') > -1) {
+                this.isDetail = true;
+            } else {
+                this.isDetail = false;
+            }
+        },
+        back() {
+            this.$router.go(-1);
         }
     }
 }
