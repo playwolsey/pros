0271140c507b793b6aa7060e685290d1f489326c
awwwesssooooome <chengpeng@yixin.im>
2017-02-22 14:20:23 +0800

    opt mobile style

--- qw/src/components/m/about.vue
+++ qw/src/components/m/about.vue
@@ -1,6 +1,5 @@
 <style>
-.about-wrap {width:500px;}
-.about-wrap:last-child {margin-left:79px;}
+.u-gray {color:#969696;}
 </style>
 
 <template>
@@ -9,16 +8,10 @@
             <h5 class="m-title">杭州全文室内设计</h5>
             <div class="m-line"></div>
             <section class="m-section">
-                <p>杭州全文室内设计是一家集合中国美术学院建筑、室内、环艺、平面设计系新锐力量的空间设计机构。</p>
+                <p>杭州全文室内设计是一家集合中国美术学院建筑、室内、环艺、平面设计系新锐力量的空间设计机构。 公司创立于2015年，致力于室内空间的设计与研究；接受室内外空间规划设计、商业品牌策划、硬装、软装设计及家具家居采购等一体化设计服务商业品牌策划、软硬装一体化，服务项目包括商业空间、餐饮会所、私人住宅等设计项目。其设计一直贯彻简约、建筑美感和空间规划为设计重点，致力于中国传统美学精神和现代设计语言的新建筑室内风格。</p>
             </section>
-            <section class="m-section">
-                            <p>公司创立于2015年，致力于室内空间的设计与研究；接受室内外空间规划设计、商业品牌策划、硬装、软装设计及家具家居采购等一体化设计服务商业品牌策划、软硬装一体化，服务项目包括商业空间、餐饮会所、私人住宅等设计项目。其设计一直贯彻简约、建筑美感和空间规划为设计重点，致力于中国传统美学精神和现代设计语言的新建筑室内风格。</p>
-            </section>
-            <section class="m-section">
-                <p>Other than design, she has directly hosted a design exhibition and  interdisciplinary cross field seminars, also host "Social Innovation Bus" to promote "Design Thinking" in Social innovation.</p>
-            </section>
-            <section class="m-section">
-                <p>Furthermore, Hsuan is passionate you might say, stubborn about designing the highest quality works. Her designs has won "Golden Pin Concept Design Award 2016 ", Merit Award of "DFA Design for Asia Awards 2016"," Macau Design Biennial 2015" and featured on "Computer Arts Magazine", "CTA" , "APD", "Etapes", "It's Nice That" and so on. In addition, her works was later on demonstrated in "National Center of Traditional Arts" and "Songshan Creative Park" and "Macau Design Centre",etc. She is now studying Interface design and Visual design in Aalto University (Helsinki, Finland).)</p>
+            <section class="m-section u-gray">
+                <p>Other than design, she has directly hosted a design exhibition and  interdisciplinary cross field seminars, also host "Social Innovation Bus" to promote "Design Thinking" in Social innovation. Furthermore, Hsuan is passionate you might say, stubborn about designing the highest quality works. Her designs has won "Golden Pin Concept Design Award 2016 ", Merit Award of "DFA Design for Asia Awards 2016"," Macau Design Biennial 2015" and featured on "Computer Arts Magazine", "CTA" , "APD", "Etapes", "It's Nice That" and so on. In addition, her works was later on demonstrated in "National Center of Traditional Arts" and "Songshan Creative Park" and "Macau Design Centre",etc. She is now studying Interface design and Visual design in Aalto University (Helsinki, Finland).)</p>
             </section>
         </arjicle>
         <article class="m-article">
--- qw/src/components/m/navbar.vue
+++ qw/src/components/m/navbar.vue
@@ -2,16 +2,16 @@
 .mnavbar-wrap {width:14.4rem;margin:0 auto;}
 .mnavbar-wrap .mnavbar {font-size:.9rem;line-height:1rem;letter-spacing:1px;padding:.36rem 0;position:relative;border-bottom:8px solid #000000;}
 .mnavbar-wrap .mnavbar .menu {font-size:.3375rem;position:absolute;right:10px;bottom:.33rem;}
-.mnavbar-wrap .mnavbar .menu span {width:.925rem;height:3px;background:#000;margin-bottom:.15rem;-webkit-transition:all 0.3s ease;-moz-transition:all 0.3s ease;transition:all 0.3s ease;display:block;}
-.mnavbar-wrap .menubox {font-size:1.35rem;line-height:1.8rem;margin-top:.3rem;}
+.mnavbar-wrap .mnavbar .menu span {width:.925rem;height:0.1125rem;background:#000;margin-bottom:.15rem;-webkit-transition:all 0.3s ease;-moz-transition:all 0.3s ease;transition:all 0.3s ease;display:block;}
+.mnavbar-wrap .menubox {font-size:1.35rem;line-height:1.35rem;margin-top:.3rem;}
 .mnavbar-wrap .menubox a {color:#DCDCDC;}
 .mnavbar-wrap .menubox a.router-link-active {color:#000;}
 .menubox-fade-enter-active {transition:all .5s cubic-bezier(0,0.5,0,1);}
 .menubox-fade-leave-active {transition:all .5s cubic-bezier(0, 0, 5, 0, 1);}
 .menubox-fade-enter, .slide-fade-leave-active {transform:translateY(-100px);opacity:0;}
-.menu.open span:nth-child(1) {-moz-transform:rotate(45deg);-webkit-transform:rotate(45deg);transform:rotate(45deg);-moz-transform-origin:0 .2rem;-webkit-transform-origin:0 .2rem;transform-origin:0 .2rem}
+.menu.open span:nth-child(1) {-webkit-transform:rotate(38deg);transform:rotate(38deg);-webkit-transform-origin:left top;transform-origin:left top;}
 .menu.open span:nth-child(2) {opacity:0;}
-.menu.open span:nth-child(3) {-moz-transform:rotate(-45deg);-webkit-transform:rotate(-45deg);transform: rotate(-45deg);-moz-transform-origin:.34rem 0;-webkit-transform-origin:.34rem 0;transform-origin:.34rem 0;}
+.menu.open span:nth-child(3) {-webkit-transform:rotate(-38deg);transform: rotate(-38deg);-webkit-transform-origin:left bottom;transform-origin:left bottom;}
 </style>
 
 <template>
