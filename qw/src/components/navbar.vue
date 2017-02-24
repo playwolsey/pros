1aad716ece59bfe47eb0a6b702981715ea1099a3
awwwesssooooome <chengpeng@yixin.im>
2017-02-24 15:41:35 +0800

    opt navbar animate

--- qw/src/components/navbar.vue
+++ qw/src/components/navbar.vue
@@ -9,17 +9,16 @@
 .navbar .navbar-brand:hover .nav-brand-front {opacity:0;}
 .navbar .nav {list-style:none;float:right;}
 .navbar .nav li {float:left;color:#969696;}
-.navbar .nav li a {font-size:16px;color:#969696;line-height:24px;padding:0 15px;}
+.navbar .nav li a {font-size:16px;font-weight:600;color:#969696;line-height:24px;padding:0 15px;}
 .navbar .nav li:nth-child(4) a {padding-right:0;}
 .navbar .nav li a:hover {color:#000;font-weight:600;}
 .navbar .nav li a.router-link-active {color:#000;font-weight:600;}
 .navbar .nav .subnav {float:right;height:24px;overflow:hidden;position:relative;line-height:24px;}
 .navbar .subnav div a {float:none;color:#969696;padding:0 5px 10px;}
-.slide-fade-enter-active {transition:all .5s ease;}
-.slide-fade-leave {transition:all .5s cubic-bezier(1.0, 0.5, 0.8, 1.0));}
+.slide-fade-enter-active {transition:all 2s ease;}
+.slide-fade-leave {transition:all 5s cubic-bezier(1.0, 0.5, 0.8, 1.0));}
 .slide-fade-enter {transform:translateX(10px);opacity:0;}
 .slide-fade-leave {transform:translate(-10px);opacity:0;}
- 
 </style>
 
 <template>
@@ -31,18 +30,18 @@
                 <h1 class="nav-brand-back">杭州全文室内设计</h1>
             </a>
             <ul class="nav"> 
+                <transition name="slide-fade">
                 <li @mouseover="show=true" @mouseleave="show=false">
                     <router-link to="/work"><span class="nav-icon-text">WORK</span></router-link>
-                    <transition name="slide-fade">
-                        <div v-if="show" class="subnav">
-                            <div>
-                                <router-link to="/interior">Interior</router-link>
-                                <router-link to="/architecture">Architecture</router-link>
-                                <router-link to="/visual">Visual Design</router-link>
-                            </div>
+                    <div v-if="show" class="subnav">
+                        <div>
+                            <router-link to="/interior">Interior</router-link>
+                            <router-link to="/architecture">Architecture</router-link>
+                            <router-link to="/visual">Visual Design</router-link>
                         </div>
-                    </transition>
+                    </div>
                 </li>  
+                </transition>
                 <li><span>/</span><router-link to="/about"><span class="nav-icon-text">ABOUT</span></router-link><span>/</span></li>  
                 <li><router-link to="/contact"><span class="nav-icon-text">CONTACT</span></router-link><span>/</span></li>
                 <li><router-link to="/job"><span class="nav-icon-text">JOB</span></router-link></li>
