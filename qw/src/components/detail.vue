5742e3138a41bf90a4f6e41362e59389ce3d3e1c
awwwesssooooome <chengpeng@yixin.im>
2017-02-27 16:36:07 +0800

    add swiper component to detail page

--- qw/src/components/detail.vue
+++ qw/src/components/detail.vue
@@ -3,6 +3,13 @@
 .detail-article-wrap p {font-weight:600;}
 .detail-img-wrap {margin-top:30px;}
 .detail-img-wrap img {width:700px;margin:10px 0;}
+#detail .copyright {color:#000;border-top:1px solid #DCDCDC;padding:20px 0 30px;}
+.swiper-wrap {width:700px;margin:80px 0 30px;position:relative;}
+.swiper-wrap img {width:213px;height:213px;}
+div.swiper-button-next, div.swiper-button-prev {width:32px;height:32px;background-size:32px 32px;position:absolute;top:-20px;}
+div.swiper-button-next {background-image:url(/res/img/next.png);}
+div.swiper-button-prev {background-image:url(/res/img/back.png);}
+.swiper-slide-next + div, .swiper-slide-active {-webkit-filter: grayscale(100%);-moz-filter: grayscale(100%);-ms-filter: grayscale(100%);-o-filter:grayscale(100%);filter:grayscale(100%);filter:gray;}
 </style>
 
 <template>
@@ -23,15 +30,31 @@
                 <li v-for="img in details.imgs"><img :src="img"></li>
             </ul>
         </article>
+        <div class="swiper-wrap">
+            <swiper :options="swiperOption">
+                <swiper-slide v-for="slide in details.imgs"><img :src="slide"></swiper-slide>
+            </swiper>
+            <div class="swiper-button-prev" slot="button-prev"></div>
+            <div class="swiper-button-next" slot="button-next"></div>
+        </div>
     </div>
+    <!--<div class="copyright">Copyrights of Hangzhou Quanwen Interior Design Co.,Ltd.</div>-->
 </div>
 </template>
 
 <script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
 export default {
     data() {
         return {
-            details: {}
+            details: {},
+            swiperOption: {
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev',
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
         }
     },
     created() {
@@ -56,6 +79,7 @@ export default {
                 console.log(response)
             });
         }
-    }
+    },
+    components: { swiper, swiperSlide }
 }
 </script>
