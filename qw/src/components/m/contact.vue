d909a6a0f2abd4703b2e49873ee839ae73330251
chengpeng <chengpengcp9@163.com>
2017-03-20 22:38:43 +0800

    anim fixed

--- qw/src/components/m/contact.vue
+++ qw/src/components/m/contact.vue
@@ -5,6 +5,7 @@
             <h5 class="m-title">杭州全文室内设计</h5>
             <div class="m-line"></div>
             <section class="m-section">
+                <p>杭州市 上城区 候潮路金都华府7幢105／杭州筑咖啡</p>
                 <p>Building 7-105, Jundu Apsrtment,</p>
                 <p>HouChao Road,</p>
                 <p>Shangcheng District,Hangzhou</p>
@@ -12,7 +13,7 @@
             <section class="m-section">
                 <p>Email：804931655@qq.com</p>
                 <p>Tel：13777403616</p>
-                <p>Wechat：全文设计</p>
+                <p>Wechat：全文设计 （个人 sunquanwen）</p>
             </section>
             <section class="m-section">
                 <p>www.quanwends.com</p>
--- qw/src/components/m/navbar.vue
+++ qw/src/components/m/navbar.vue
@@ -3,16 +3,15 @@
 .mnavbar-wrap .mnavbar {font-size:.9rem;line-height:1rem;letter-spacing:1px;padding:.36rem 0;position:relative;border-bottom:8px solid #000000;}
 .mnavbar-wrap .mnavbar .back {width:1.6rem;height:1.6rem;background:url(/res/img/back.png) no-repeat center center;background-size:100% 100%;margin:.7rem 0 .7rem;}
 .mnavbar-wrap .mnavbar .menu {font-size:.3375rem;position:absolute;right:0;bottom:.33rem;cursor:pointer;}
-.mnavbar-wrap .mnavbar .menu span {width:.925rem;height:0.1125rem;background:#000;margin-bottom:.196rem;-webkit-transition:all 0.3s ease;-moz-transition:all 0.3s ease;transition:all 0.3s ease;display:block;}
+.mnavbar-wrap .mnavbar .menu span {width:.925rem;height:0.1125rem;background:#000;margin-bottom:.196rem;-webkit-transition:all 0.3s ease;-moz-transition:all 0.3s ease;transition:all 0.3s ease;display:block;cursor:pointer;}
 .menu.open span:nth-child(1) {-webkit-transform:rotate(45deg);transform:rotate(45deg);-webkit-transform-origin:left top;transform-origin:left top;}
 .menu.open span:nth-child(2) {opacity:0;}
 .menu.open span:nth-child(3) {-webkit-transform:rotate(-45deg);transform: rotate(-45deg);-webkit-transform-origin:left bottom;transform-origin:left bottom;}
-.mnavbar-wrap .menubox {font-size:1.35rem;line-height:1.35rem;margin-top:.5rem;font-weight:600;}
+.mnavbar-wrap .menubox {font-size:1.35rem;line-height:1.35rem;margin-top:.5rem;font-weight:600;opacity:0;}
 .mnavbar-wrap .menubox a {color:#DCDCDC;}
 .mnavbar-wrap .menubox a.router-link-active {color:#000;}
-.menubox-fade-enter-active {transition:all .5s cubic-bezier(0,0.5,0,1);}
-.menubox-fade-leave-active {transition:all .5s cubic-bezier(0, 0, 5, 0, 1);}
-.menubox-fade-enter, .slide-fade-leave-active {transform:translateY(-100px);opacity:0;}
+.mnavbar-wrap .menubox.show {animation:fadeIn 1s ease;-webkit-animation:fadeIn 1s ease;animation-fill-mode:forwards;}
+.mnavbar-wrap .menubox.hidden {animation:fadeOut 1s ease;-webkit-animation:fadeOut 1s ease;animation-fill-mode:forwards;}
 </style>
 
 <template>
@@ -29,22 +28,20 @@
             </div>
             <div class="back" v-if="isDetail||isWork" @click="back"></div>
         </div>
-        <transition name="menubox-fade">
-            <div class="menubox" v-if="show">
-                <ul v-if="!isWork">
-                    <li><router-link to="/m/work">Work</router-link></li>
-                    <li><router-link to="/m/about">About</router-link></li>
-                    <li><router-link to="/m/contact">Contact</router-link></li>
-                    <li><router-link to="/m/job">Job</router-link></li>
-                </ul>
-                <ul v-if="isWork">
-                    <li><router-link to="/m/interior">Interior</router-link></li>
-                    <li><router-link to="/m/architecture">Architecture</router-link></li>
-                    <li><router-link to="/m/visual">Visual Design</router-link></li>
-                    <li><router-link to="/m/all">All</router-link></li>
-                </ul>
-            </div>
-        </transition>
+        <div class="menubox" ref="menubox">
+            <ul v-if="!isWork">
+                <li><router-link to="/m/work">Work</router-link></li>
+                <li><router-link to="/m/about">About</router-link></li>
+                <li><router-link to="/m/contact">Contact</router-link></li>
+                <li><router-link to="/m/job">Job</router-link></li>
+            </ul>
+            <ul v-if="isWork">
+                <li><router-link to="/m/interior">Interior</router-link></li>
+                <li><router-link to="/m/architecture">Architecture</router-link></li>
+                <li><router-link to="/m/visual">Visual Design</router-link></li>
+                <li><router-link to="/m/all">All</router-link></li>
+            </ul>
+        </div>
     </div>
 </div>
 </template>
@@ -57,48 +54,63 @@ export default {
             menuClass: 'menu',
             openClass: 'open',
             show: false,
+            scroll: false,
             isWork: false,
             isDetail: false
         }
     },
     watch: {
-        '$route': 'getIsDetail'
+        $route(data, old) {
+            let path = this.$route.path;
+            this.getIsDetail()
+            this.show = false
+            this.scroll = false
+            this.$refs.menubox.className = 'menubox'
+
+                this.$emit('slideDown', this.scroll)
+        }
     },
     created() {
-        this.getIsDetail();
+        this.getIsDetail()
     },
     methods: {
         getIsDetail() {
             let path = this.$route.path;
 
             if (path.indexOf('detail') > -1) {
-                this.isDetail = true;
-                this.show = false;
+                this.isDetail = true
+                this.show = false
             } else {
                 this.isDetail = false;
             }
 
             if (path.indexOf('work') > -1 || path.indexOf('interior') > -1 || path.indexOf('architecture') > -1 || path.indexOf('visual') > -1 || path.indexOf('all') > -1) {
-                this.isWork = true;
+                this.isWork = true
             } else {
-                this.isWork = false;
+                this.isWork = false
             }
         },
         back() {
             let path = this.$route.path;
 
             if (path.indexOf('detail') > -1) {
-                this.$router.go(-1);
-                //this.show = true;
+                this.$router.go(-1)
             }
 
             if (path.indexOf('work') > -1 || path.indexOf('interior') > -1 || path.indexOf('architecture') > -1 || path.indexOf('visual') > -1 || path.indexOf('all') > -1) {
-                this.$router.push({path: '/m'});
+                this.$router.push({path: '/m'})
             }
         },
         showNavbar() {
             this.show = !this.show
-            this.$emit('slideDown', this.show)
+            this.scroll = !this.scroll
+            this.$emit('slideDown', this.scroll)
+
+            if (this.scroll) {
+                this.$refs.menubox.className = 'menubox show'
+            } else {
+                this.$refs.menubox.className = 'menubox hidden'
+            }
         }
     }
 }
